c68e80d40f192838fb46e3816e037934bdcb9d42
ANil <apanthi21@gmail.com>
2015-03-12 01:28:22 +0545

    Reverted back Due to an error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,6 +38,6 @@ class PostsController < ApplicationController
     
   end
   def post_params
-    params.require(:post).permit(:title, :link, :description, :image)
+    params.require(:post).permit(:title, :link, :description)
   end
 end
